c3b69a24b95a87f664015169933b1ad2dbd338b6
Shoaib Zeshan <szeeshan707@gmail.com>
2022-09-03 18:29:59 +0530

    destructing api

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
+import fetchProducts from './fetchProducts.js';
 import { getStorageItem, setStorageItem } from './utils.js';
 let store = [];
-const setupStore = () => {};
-const findProduct = () => {};
+const setupStore = (products) => {
+    store = products.map((product) => {
+
+        const { id, fields: { featured, name, price, company, color, image: img } } = product;
+        const image = img[0].thumbnails.large.url
+
+        return { id, featured, name, price, company, color, image }
+    })
+};
+const findProduct = () => { };
 export { store, setupStore, findProduct };
