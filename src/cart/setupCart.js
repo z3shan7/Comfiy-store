0d37df82f727d403d38c24fc80714bb2933b89f3
Shoaib Zeshan <szeeshan707@gmail.com>
2022-09-23 22:12:19 +0530

    add to cart functionality added

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,8 +10,59 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
-export const addToCart = (id) => {
-  // console.log(id)
-  openCart()
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
 
+let cart = getStorageItem('cart');
+
+export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    // add item to the the
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add item to the DOM;
+    addToCartDOM(product);
+  } else {
+    // update values
+  }
+  // add one to the item count
+  displayCartItemCount();
+  // display cart totals
+  displayCartTotal();
+  // set cart in local storage
+  setStorageItem('cart', cart);
+  //more stuff coming up
+  openCart();
 };
+function displayCartItemCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount);
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+}
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  cartTotalDOM.textContent = `Total : ${formatPrice(total)} `;
+}
+function displayCartItemsDOM() {
+  cart.forEach((cartItem) => {
+    addToCartDOM(cartItem);
+  });
+}
+function setupCartFunctionality() { }
+const init = () => {
+  // display amount of cart items
+  displayCartItemCount();
+  // display total
+  displayCartTotal();
+  // add all cart items to the dom
+  displayCartItemsDOM();
+  // setup cart functionality
+  setupCartFunctionality();
+};
+init();
--- src/store.js
+++ src/store.js
@@ -12,5 +12,8 @@ const setupStore = (products) => {
     setStorageItem('store', store)
 };
 // console.log(store)
-const findProduct = () => { };
+const findProduct = (id) => {
+    let product = store.find((product) => product.id === id)
+    return product
+};
 export { store, setupStore, findProduct };
